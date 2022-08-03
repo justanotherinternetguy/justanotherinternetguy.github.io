cd0321b13f5205793cb595c84a39c30c192eb975
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-08-02 22:06:17 -0400

    e

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-4xl">
+        ive kinda just given up with webdev at the moment, just look at my
+        github or something, idk
+      </h1>
+      <h1 className="text-blue-600 text-4xl">
+        <a href="https://github.com/justanotherinternetguy" target="_blank">
+          https://github.com/justanotherinternetguy
+        </a>
+      </h1>
+
+      <h1 className="text-red-500 text-4xl mt-20">
+        my other (poorly) designed websites:
+      </h1>
+      <h1 className="text-blue-600 text-4xl">
+        <a href="https://jaigv2.surge.sh/" target="_blank">
+          https://jaigv2.surge.sh/
+        </a>
+      </h1>
+      <h1 className="text-blue-600 text-4xl">
+        <a href="https://justanotherinternetguy.surge.sh/" target="_blank">
+          https://justanotherinternetguy.surge.sh/
+        </a>
+      </h1>
+    </div>
+  );
+}
